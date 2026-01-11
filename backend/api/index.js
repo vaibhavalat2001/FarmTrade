38166264fdc91a45b2c1462e722f899a65cea9c8
vaibhav gajanan alat <vaibhavalat@gmail.com>
2026-01-11 16:45:29 +0530

    add

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,2 +1,10 @@
+app.get("/env", (req, res) => {
+  res.json({
+    MONGODB_URI: process.env.MONGODB_URI ? "OK" : "NOT SET",
+    JWT_SECRET: process.env.JWT_SECRET ? "OK" : "NOT SET"
+  });
+});
+
+
 const app = require("../server");
 module.exports = app;
